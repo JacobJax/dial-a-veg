d69f7ffa914aaf386abf6021017825f3bfc9606e
Jacob Odero <jackjax617@gmail.com>
2020-12-14 22:05:48 +0300

    create user login

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+
+<footer>
+<p>POWERED BY: Copyright GROUP A &copy;2020</p>
+</footer>
+</body>
---
+++ includes/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dial a veg</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="../design/css/style.css">
+</head>
+<body>
+    <nav>
+        <div class="nav-left">
+            <div class="nav-logo">
+                <h3>Dial a veg</h3>
+            </div>
+            <div class="categories">
+                <ul>
+                    <li>
+                        <a href="#" class="categories-head">Categories</a>
+                        <ul class="dropdown" style="padding-top: 25px;">
+                            <li><a href="">Leafy greens</a></li>
+                            <li><a href="">Cruferous</a></li>
+                            <li><a href="">Marrow</a></li>
+                            <li><a href="">Root</a></li>
+                            <li><a href="">Edible plants</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav-items">
+            <ul>
+                <div class="common-items">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contat us</a></li>
+                </div>
+                <div class="call-to-action">
+                    <li><a href="#" class="seller-btn">Become a seller</a></li>
+                    <li><a href="#" class="login-btn">Log in</a></li>
+                </div>                
+            </ul>
+        </div>
+    </nav>
+
